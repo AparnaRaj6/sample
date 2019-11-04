a06fb7f81dfc73e48d4ef3d588c7292da2e10a43
AparnaRaj6 <57254753+AparnaRaj6@users.noreply.github.com>
2019-11-04 18:24:02 +0530

    sample_test

--- try.py
+++ try.py
@@ -1,3 +1,14 @@
 print('Hello World!')
 print('Good Morning')
 print('Have a nice day')
+a=2
+
+
+a=5
+if(a==2):
+    print('even')
+else:
+    print('odd')
+
+print('what a lovely day')
+print('not successful')
