cc58f2bdf1659238d69ae9c7a9f94c2aeabac184
Deepanshu Aggarwal <deepanshu.aggarw@tigeranalytics.local>
2019-11-05 12:49:45 +0530

    first try

--- try.py
+++ try.py
@@ -1,3 +1,11 @@
 print('Hello World!')
 print('Good Morning')
 print('Have a nice day')
+print(2+5)
+import pandas as pd
+import numpy as np
+
+data = pd.DataFrame({'col_1': [3, 2, 1, 0, 5 , 8 , 9], 'col_2': ['a', 'b', 'c', 'd','e','f', 'g']})
+data.head()
+print('Deepanshu is a genius')
+print('flower')
