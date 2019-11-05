552f9cb12dda03c0bf553edc05cf04e3580d4e5d
AparnaRaj6 <57254753+AparnaRaj6@users.noreply.github.com>
2019-11-05 11:50:14 +0530

    finally commited after a long time

--- try.py
+++ try.py
@@ -2,13 +2,17 @@ print('Hello World!')
 print('Good Morning')
 print('Have a nice day')
 a=2
-
-
+b=10
+a=7
 a=5
-if(a==2):
+a='b'
+if(a==2 & b==10):
     print('even')
 else:
     print('odd')
 
 print('what a lovely day')
 print('not successful')
+print('--------')
+print('in dev')
+print('a')
